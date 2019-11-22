314a288a7ad11fd65aad3167b548cbea57f8401d
Jean-Bertrand Couret <jb.couret@gmail.com>
2019-11-22 19:13:35 +0100

    Change email by env email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'mediswap.herokuapp' }
+  config.action_mailer.default_url_options = { host: 'mediswap.fr' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",

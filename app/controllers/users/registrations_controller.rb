9b0a1e06f95de8641df6f45f53b874f2b2a4bf0e
Jean-Bertrand Couret <jb.couret@gmail.com>
2019-12-09 11:35:50 +0100

    Update Users' views after controllers' architecture changes

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
+
+  # POST /resource
+  # def create
+  #   super
+  # end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :title,
+      :first_name,
+      :last_name,
+      :account_type,
+      :profession,
+      :professional_id_number
+    ])
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :title,
+      :first_name,
+      :last_name,
+      :avatar_id,
+      :remove_avatar_id,
+      :birth_date,
+      :birth_place,
+      :account_type,
+      :personnal_phone_number,
+      :personnal_address,
+      :personnal_additional_address,
+      :personnal_postal_code_address,
+      :personnal_city_address,
+      :profession,
+      :professional_status,
+      :structure_name,
+      :professional_id_number,
+      :urssaf_number,
+      :council_number,
+      :council_location,
+      :council_email,
+      :professional_phone_number,
+      :professional_address,
+      :professional_additional_address,
+      :professional_postal_code_address,
+      :professional_city_address
+    ])
+  end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+  
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+  
+  # Redirect user to his edit view after edit
+  def after_update_path_for(resource)
+    edit_user_registration_path(resource)
+  end
+
+end

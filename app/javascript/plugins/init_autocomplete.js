ab1aca26784ffc8564e4d8c95d8a8378c9188c49
Jean-Bertrand Couret <jb.couret@gmail.com>
2019-12-11 17:48:57 +0100

    Limit geocode search with French cities

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,14 +1,19 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInputs = document.querySelectorAll('.js-navbarAddressSearch');
+  const addressInputs = document.querySelectorAll('.js-autoCitySearch');
   if (addressInputs.length > 0) {
     addressInputs.forEach(addressInput =>
       places({ 
-          container: addressInput,
-          language: 'fr',
-          country: 'fr',
-          type: ['address', 'city'],
+        container: addressInput,
+        language: 'fr',
+        countries: 'fr',
+        type: 'city',
+        templates: {
+          value: function(suggestion) {
+            return suggestion.name;
+          }
+        }
       })
     );
   }

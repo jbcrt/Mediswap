75cc76d1189a62b5484c69dbefe911bac0aa3239
Jean-Bertrand Couret <jb.couret@gmail.com>
2020-01-30 22:42:00 +0100

    Fix host for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'mediswap.net' }
+  config.action_mailer.default_url_options = { host: 'www.mediswap.net' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",

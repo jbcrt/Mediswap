92cb8566d4f76b34a69db5af3a5d802f404d50bc
Jean-Bertrand Couret <jb.couret@gmail.com>
2020-05-20 23:18:16 +0200

    Add 'edit', 'update' and 'destroy' for offers + index for users' offers + rework model validations for update + clean offers forms js

--- app/controllers/account/offers_controller.rb
+++ app/controllers/account/offers_controller.rb
@@ -2,19 +2,28 @@ class Account::OffersController < ApplicationController
   before_action :set_offer, only: [:edit, :update, :destroy]
 
   def index
-    @offers = policy_scope(Offer).where(user: current_user)
+    @offers = policy_scope(Offer).where(user_id: current_user).order(created_at: :desc)
   end
 
   def edit
-    @offer
   end
 
   def update
-    @offer
+    if @offer.update(offer_params)
+      flash[:notice] = "Votre annonce a bien été modifiée."
+      redirect_to edit_account_offer_path(@offer)
+    else
+      render :edit
+    end
   end
-
+  
   def destroy
-    @offer
+    if @offer.destroy
+      flash[:notice] = "Votre annonce a bien été supprimée."
+      redirect_to account_offers_path
+    else
+      flash[:alert] = "Une erreur est survenue, votre annonce n'a pas pu être supprimée."
+    end
   end
 
   private
@@ -25,6 +34,106 @@ class Account::OffersController < ApplicationController
   end
 
   def offer_params
-    params.require(:offer).permit(:title, :starts_at, :ends_at, :offer_type, :description, :retrocession, :vehicle, :housing_possibility, :secretariat)
+    if params[:replacement]
+      params.require(:replacement).permit(
+        :title,
+        :description,
+        :street,
+        :additional_address,
+        :department,
+        :zipcode,
+        :city,
+        :starts_at,
+        :ends_at,
+        :working_time,
+        :retrocession,
+        :daily_medical_acts_number,
+        :on_call,
+        :vehicle_required,
+        :visit,
+        :sector,
+        :secretariat,
+        :software_used,
+        :housing_possibility
+      )
+    elsif params[:employment]
+      params.require(:employment).permit(
+        :title,
+        :description,
+        :street,
+        :additional_address,
+        :department,
+        :zipcode,
+        :city,
+        :profession,
+        :working_time,
+        :has_salary_set,
+        :salary,
+        :salary_period,
+        :candidate_job_experience,
+        :candidate_description
+      )
+    elsif params[:collaboration]
+      params.require(:collaboration).permit(
+        :title,
+        :description,
+        :street,
+        :additional_address,
+        :department,
+        :zipcode,
+        :city,
+        :retrocession,
+        :daily_medical_acts_number,
+        :on_call,
+        :vehicle_required,
+        :visit,
+        :sector,
+        :secretariat,
+        :software_used,
+        :housing_possibility
+      )
+    elsif params[:patient_transfer]
+      params.require(:patient_transfer).permit(
+        :title,
+        :description,
+        :street,
+        :additional_address,
+        :department,
+        :zipcode,
+        :city,
+        :has_selling_price_set,
+        :selling_price,
+        :revenues,
+        :premises_availability,
+        :premises_price,
+        :premises_rent,
+        :premises_size,
+        :premises_rooms_number,
+        :premises_furnished,
+        :premises_equipment,
+        :premises_has_parking,
+        :accessible_premises
+      )
+    elsif params[:establishment]
+      params.require(:establishment).permit(
+        :title,
+        :description,
+        :street,
+        :additional_address,
+        :department,
+        :zipcode,
+        :city,
+        :profession,
+        :premises_price,
+        :premises_rent,
+        :premises_size,
+        :premises_rooms_number,
+        :premises_furnished,
+        :premises_equipment,
+        :premises_has_parking,
+        :accessible_premises
+      )
+    end
   end
+
 end
--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,6 +1,8 @@
 class Employment < Offer
     belongs_to :user
 
+    before_validation :set_conditional_values
+
     enum contract_type: {
         contrat_assitant_hopitaux: "Assistant des hopitaux",
         contrat_duree_determinee: "Contrat à durée déterminée",
@@ -34,7 +36,7 @@ class Employment < Offer
     validates :candidate_description, length: { in: 1..2500 }, allow_blank: true
 
     # Les champs dont les validations sont conditionnées à la valeur de "has_salary_set"
-    with_options if: Proc.new { |a| a.has_salary_set } do |employment_offer_with_salary_set|
+    with_options if: Proc.new { |a| a.has_salary_set? } do |employment_offer_with_salary_set|
         employment_offer_with_salary_set.validates :salary, numericality: { greater_than: 0 }
         employment_offer_with_salary_set.validates :salary_period, inclusion: { in: Employment.salary_periods.keys }
     end
@@ -69,5 +71,14 @@ class Employment < Offer
     validates :premises_equipment, absence: true
     validates :premises_has_parking, absence: true
     validates :accessible_premises, absence: true
+
+    private
+
+    def set_conditional_values
+        if self.has_salary_set == false
+          self.salary = nil
+          self.salary_period = nil
+        end
+    end
 
 end

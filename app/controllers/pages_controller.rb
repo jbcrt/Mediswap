e7c4fc4ebcaf0b93c316d94c9fa06a80f2743c82
Jean-Bertrand Couret <jb.couret@gmail.com>
2020-06-13 14:48:04 +0200

    add new_offer page exception for skip_before_action in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, except: [:new_offer] 
   
   def home
     @offer_count = Offer.count

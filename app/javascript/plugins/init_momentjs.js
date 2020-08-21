15d36a1150c39588318c07da1484b80f16d980eb
Jean-Bertrand Couret <jb.couret@gmail.com>
2020-08-22 01:14:58 +0200

    Fix momentjs for Turbolinks + fix map display for Turbolinks + minor change in offers views

--- app/javascript/plugins/init_momentjs.js
+++ app/javascript/plugins/init_momentjs.js
@@ -4,9 +4,13 @@ moment.locale('fr');
 const initMomentSinceDuration = () => {
     const durations = document.querySelectorAll(".js-sinceDuration")
     if (durations.length > 0) {
-        durations.forEach(duration =>
-          duration.innerHTML = moment(duration.innerHTML).fromNow()
-        );
+        durations.forEach(duration => {
+            if (duration.dataset.sinceDurationSet === undefined) {
+              duration.innerHTML = moment(duration.innerHTML).fromNow()
+              // Idempotent transformation for turbolinks
+              duration.dataset.sinceDurationSet = true
+            }
+        });
       }
 }
 
